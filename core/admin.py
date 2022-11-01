c111de75735eb218fb6540cd9895f6eb2193f424
Dominik <46494682+ludzik940@users.noreply.github.com>
2022-11-01 19:54:54 +0100

    Delete core directory

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Links as Link
-
-# Register your models here.
-
-admin.site.register(Link)
--- core/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_file_size(value):
-    filesize = value.size
-
-    if filesize > 2621440:
-        raise ValidationError('The maximum file size that can be uploaded is 2MB')
-    else:
-        return value
